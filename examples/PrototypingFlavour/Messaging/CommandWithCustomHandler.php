98822974cee6b8f3d1ba34016c3c562129c810a5
codeliner <kontakt@codeliner.ws>
2018-11-27 20:37:59 +0100

    Allow a preprocessor to stop dispatch

---
+++ examples/PrototypingFlavour/Messaging/CommandWithCustomHandler.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the proophsoftware/event-machine.
+ * (c) 2017-2018 prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophExample\PrototypingFlavour\Messaging;
+
+use Prooph\EventMachine\EventMachine;
+use Prooph\EventMachine\EventMachineDescription;
+use Prooph\EventMachine\JsonSchema\JsonSchema;
+
+final class CommandWithCustomHandler implements EventMachineDescription
+{
+    public const CMD_DO_NOTHING = 'DoNothing';
+    public const NO_OP_HANDLER = 'NoOpHandler';
+
+    public static function describe(EventMachine $eventMachine): void
+    {
+        $eventMachine->registerCommand(self::CMD_DO_NOTHING, JsonSchema::object(['msg' => JsonSchema::string()]));
+        $eventMachine->preProcess(self::CMD_DO_NOTHING, self::NO_OP_HANDLER);
+    }
+}
--- tests/EventMachinePrototypingFlavourTest.php
+++ tests/EventMachinePrototypingFlavourTest.php
@@ -11,13 +11,18 @@ declare(strict_types=1);
 
 namespace Prooph\EventMachineTest;
 
+use Prooph\Common\Messaging\Message as ProophMessage;
+use Prooph\EventMachine\Commanding\CommandPreProcessor;
+use Prooph\EventMachine\Container\EventMachineContainer;
 use Prooph\EventMachine\EventMachine;
+use Prooph\EventMachine\Messaging\Message;
 use Prooph\EventMachine\Messaging\MessageDispatcher;
 use Prooph\EventMachine\Persistence\DocumentStore;
 use Prooph\EventMachine\Runtime\Flavour;
 use Prooph\EventMachine\Runtime\PrototypingFlavour;
 use ProophExample\PrototypingFlavour\Aggregate\UserDescription;
 use ProophExample\PrototypingFlavour\Aggregate\UserState;
+use ProophExample\PrototypingFlavour\Messaging\CommandWithCustomHandler;
 use ProophExample\PrototypingFlavour\Messaging\MessageDescription;
 use ProophExample\PrototypingFlavour\ProcessManager\SendWelcomeEmail;
 use ProophExample\PrototypingFlavour\Projector\RegisteredUsersProjector;
@@ -82,5 +87,48 @@ class EventMachinePrototypingFlavourTest extends EventMachineTestAbstract
         self::expectExceptionMessage('At least one EventMachineDescription contains a Closure and is therefor not cacheable!');
 
         $eventMachine->compileCacheableConfig();
+    }
+
+    /**
+     * @test
+     */
+    public function it_stops_dispatch_if_preprocessor_sets_metadata_flag()
+    {
+        $eventMachine = new EventMachine();
+
+        $eventMachine->load(CommandWithCustomHandler::class);
+
+        $noOpHandler = new class() implements CommandPreProcessor {
+            private $msg;
+
+            /**
+             * {@inheritdoc}
+             */
+            public function preProcess(ProophMessage $message): ProophMessage
+            {
+                if ($message instanceof Message) {
+                    $this->msg = $message->get('msg');
+                }
+
+                return $message->withAddedMetadata(EventMachine::CMD_METADATA_STOP_DISPATCH, true);
+            }
+
+            public function msg(): ?string
+            {
+                return $this->msg;
+            }
+        };
+
+        $eventMachine->initialize(new EventMachineContainer($eventMachine));
+
+        $eventMachine->bootstrapInTestMode([], [
+            CommandWithCustomHandler::NO_OP_HANDLER => $noOpHandler,
+        ]);
+
+        $eventMachine->dispatch(CommandWithCustomHandler::CMD_DO_NOTHING, [
+            'msg' => 'test',
+        ]);
+
+        $this->assertEquals('test', $noOpHandler->msg());
     }
 }
